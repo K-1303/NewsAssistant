ae498aa8d6fbed9fb5ef29007e8e2bbe0445bcd8
kavyadua~ <kavyadua03@gmail.com>
2024-03-04 22:48:52 +0530

    minor changes ```

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 from langchain.document_loaders import UnstructuredURLLoader
 import requests
-from dotenv import load_dotenv
-import os
-from flask_cors import CORS  
+#from flask_cors import CORS  
 from flask import render_template_string
-
-load_dotenv()
-HuggingFaceApiKey = os.getenv("HuggingFaceApiKey")
 
 
 app = Flask(__name__)
-CORS(app, origins='chrome-extension://ooiefbeaelfhmbchpihnfblpkcaclkkf')  
+#CORS(app, origins='ADD_EXTENSION_URL_HERE')  
 
 # Initialize an empty list to store news URLs
-news_urls_question = ["", ""]
 news_summarization_url = ""
 
-data = ""
 data1 = ""
 
 @app.route('/')
 def index():
-    return render_template('index.html', news_urls=news_urls_question)
+    return render_template('index.html')
 
 
 @app.route('/summarize', methods=['POST'])
@@ -37,11 +30,10 @@ def add_urls_summarization():
             data1 = loader.load()
             data1 = str(data1)
 
-            headers = {"Authorization": HuggingFaceApiKey}
             
             API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
             def query(payload):
-                response = requests.post(API_URL, headers=headers, json=payload)
+                response = requests.post(API_URL, json=payload)
                 return response.json()
             data_ = query(
                 {
@@ -52,10 +44,10 @@ def add_urls_summarization():
             summary = data_[0]['summary_text']
             # Get the summary text
 
-            return render_template_string(open("templates/index.html").read(), news_urls=news_urls_question, summary=summary)  # Render the index.html template with summary data
+            return render_template_string(open("templates/index.html").read(), summary=summary)  # Render the index.html template with summary data
 
     # If it's a GET request or the URL is empty, render the edit_urls.html template
-    return render_template('index.html', news_urls=news_urls_question)
+    return render_template('index.html')
 
 
 # For API requests
